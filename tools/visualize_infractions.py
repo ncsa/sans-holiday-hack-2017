70c9f2a9cec92ccfbd72007568277f8fd8d52046
Vlad Grigorescu <grigorescu@gmail.com>
2017-12-11 17:08:50 -0600

    Messing with various ways to visualize this.

--- tools/visualize_infractions.py
+++ tools/visualize_infractions.py
@@ -2,7 +2,6 @@
 import json
 from operator import itemgetter
 
-
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
@@ -17,25 +16,34 @@ m = {
 COLOR = '\033[{}m'
 END = '\033[0m'
 def colorify(char):
-    #return m[n]
+    return m[char]
     n = int(char)
     color_code = COLOR.format(30+n)
-    char = " O X ."[n]
-    return color_code + char + END
+    char = " X X X"[n]
+    #return color_code + char + END
+    return char
     #return ("O" * n) + (" " * (5-n))
 
 def display(line):
     print(''.join(colorify(c) for c in line))
 
+def factor(num):
+    return [x for x in range(5, num) if num % x == 0 and num/x > 4]
+    
 def main():
     with open("infractions.json") as f:
         infractions = json.load(f)
 
-    infractions.sort(key=itemgetter("date"), reverse=True)
+    deduped_infractions = list({v['date']: v for v in infractions}.values())
+    # Remove Whos
+    if True:
+        deduped_infractions = [x for x in deduped_infractions if 'Who' not in x['name']]
 
-    msg = [int(i['severity']) for i in infractions]
+    deduped_infractions.sort(key=itemgetter("date"), reverse=True)
 
-    for cs in range(5,220):
+    msg = [i['status'] for i in deduped_infractions]
+
+    for cs in factor(len(msg)):
         for chunk in chunks(msg, cs):
             display(chunk)
         print()
