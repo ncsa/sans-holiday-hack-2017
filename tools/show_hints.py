2d953487ef17ec1efc7b90c61ba99488ebc15c33
Justin Azoff <justin.azoff@gmail.com>
2017-12-15 10:03:37 -0600

    hints stuff

---
+++ tools/show_hints.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import collections
+import json
+import sys
+
+filename = sys.argv[1]
+
+with open(filename) as f:
+    data = json.load(f)
+
+tokens = data['mytokens']
+
+all_hints = collections.defaultdict(list)
+
+for t in tokens:
+    hints = [m for m in t['metadata'] if m['type'] == 'hint']
+    for h in hints:
+        all_hints[h['name']].append(h)
+
+for name, hints in all_hints.items():
+    print(name)
+    print("=" * 20)
+    for h in hints:
+        print("{title}: {body}".format(**h))
+    print()
