718584b9e2d8ae5985dae3eecf84bc71a8c67086
Justin Azoff <justin.azoff@gmail.com>
2017-12-16 15:19:27 -0500

    solve #5

--- tools/visualize_infractions.py
+++ tools/visualize_infractions.py
@@ -1,54 +1,21 @@
 #!/usr/bin/env python3
 import json
 from operator import itemgetter
+from collections import defaultdict
 
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+WANT = set(['Aggravated pulling of hair', 'Throwing rocks (at people)', 'Throwing rocks (non-person target)'])
 
-m = {
-    'closed': 'O',
-    'pending': '.',
-    'open': ' ',
-}
-
-COLOR = '\033[{}m'
-END = '\033[0m'
-def colorify(char):
-    return m[char]
-    n = int(char)
-    color_code = COLOR.format(30+n)
-    char = " X X X"[n]
-    #return color_code + char + END
-    return char
-    #return ("O" * n) + (" " * (5-n))
-
-def display(line):
-    print(''.join(colorify(c) for c in line))
-
-def factor(num):
-    return [x for x in range(5, num) if num % x == 0 and num/x > 4]
-    
 def main():
-    with open("infractions.json") as f:
+    byname = defaultdict(set)
+    with open("../output/infractions.json") as f:
         infractions = json.load(f)
 
-    deduped_infractions = list({v['date']: v for v in infractions}.values())
-    # Remove Whos
-    if True:
-        deduped_infractions = [x for x in deduped_infractions if 'Who' not in x['name']]
+    for i in infractions:
+        byname[i['name']].add(i['title'])
 
-    deduped_infractions.sort(key=itemgetter("date"), reverse=True)
-
-    msg = [i['status'] for i in deduped_infractions]
-
-    for cs in factor(len(msg)):
-        for chunk in chunks(msg, cs):
-            display(chunk)
-        print()
-        print()
-        print()
+    for n, titles in byname.items():
+        if len(titles & WANT) > 1:
+            print(n,titles)
 
 if __name__ == "__main__":
    main() 
