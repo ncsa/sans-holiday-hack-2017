ff6618f8cbccfe7ade770c596c4282b83000599f
Justin Azoff <justin.azoff@gmail.com>
2017-12-17 20:37:46 -0500

    add non-working link example

--- tools/phish.py
+++ tools/phish.py
@@ -3,10 +3,11 @@ from ewa import EWA
 
 def phish():
     f = "jessica.claus@northpolechristmastown.com"
-    to = "alabaster.snowball@northpolechristmastown.com"
+    to = "admin@northpolechristmastown.com"
     subject = "gingerbread cookie recipe"
 
     body = "hello!"
+    body = "gingerbread cookie recipe\nATTACHED FILE DOWNLOAD HERE: http://10.142.0.11:8686/afile.docx"
 
     m = EWA()
     resp = m.send_mail(f, to, subject, body)
