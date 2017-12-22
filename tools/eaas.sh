525ae16ab5f8817e14d16f6741754594ad262a2d
Justin Azoff <justin.azoff@gmail.com>
2017-12-22 11:52:56 -0500

    fix proxy port

--- tools/eaas.sh
+++ tools/eaas.sh
@@ -1,2 +1,2 @@
-http --session=eaas --form -v --proxy=http:socks5://@localhost:32080 POST http://eaas.northpolechristmastown.com/Home/DisplayXml file@../support_files/ElfHack.xml submit=Upload
-http --session=eaas --form -v --proxy=http:socks5://@localhost:32080 GET http://eaas.northpolechristmastown.com/Home/DisplayXml
+http --session=eaas --form -v --proxy=http:socks5://@localhost:31080 POST http://eaas.northpolechristmastown.com/Home/DisplayXml file@../support_files/ElfHack.xml submit=Upload
+http --session=eaas --form -v --proxy=http:socks5://@localhost:31080 GET http://eaas.northpolechristmastown.com/Home/DisplayXml
